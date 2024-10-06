c3434f83eb558ce7c93727717349a668b444eecb
Ashley Mlambo <ashleymlambo@gmail.com>
2024-10-05 22:16:03 -0400

    Update for new profile page

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -4,13 +4,14 @@ import { ChatComponent } from './chat/chat.component';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import { GroupListComponent } from './group-list/group-list.component';
 import { SignupComponent } from './signup/signup.component';
+import { ProfileComponent } from './profile/profile.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/chat', pathMatch: 'full' },
-    { path: 'login', component: LoginComponent },
-    { path: 'signup', component: SignupComponent },
-    { path: 'chat', component: ChatComponent },
-    { path: 'groups', component: GroupListComponent },
-    { path: 'admin', component: AdminDashboardComponent },
-
+  { path: '', redirectTo: '/chat', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'chat', component: ChatComponent },
+  { path: 'groups', component: GroupListComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'admin', component: AdminDashboardComponent },
 ];
